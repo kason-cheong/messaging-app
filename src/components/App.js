748647eae2a6044dcc22639cd05addcd6cfc8113
kason cheong <kasonck@yahoo.com>
2023-09-24 23:10:22 +1300

    Update login component and generating new ID

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
+import Login from './Login'
+import React, { useState } from 'react'
+
+
 function App() {
-  return 'Hello World'
+  const [id, setId] = useState()
+
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  )
 }
 
 export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,37 @@
+import { useRef } from 'react'
 import React from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+import { v4 as uuidV4 } from 'uuid'
 
-export default function Login() {
-  return <div></div>
+export default function Login({ onIdSubmit }) {
+  const idRef = useRef()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onIdSubmit(idRef.current.value)
+  }
+
+  function createNewId() {
+    onIdSubmit(uuidV4())
+  }
+
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: '100vh' }}
+    >
+      <Form onSubmit={handleSubmit} className="w-100">
+        <Form.Group>
+          <Form.Label>Enter your ID</Form.Label>
+          <Form.Control type="text" ref={idRef} required />
+        </Form.Group>
+        <Button type="submit" className="mr-2">
+          Login
+        </Button>
+        <Button variant="secondary" onClick={createNewId}>
+          Create a new ID
+        </Button>
+      </Form>
+    </Container>
+  )
 }
